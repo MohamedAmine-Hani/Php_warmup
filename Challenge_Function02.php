542d91edb41f1554ae8bc82c10169fd35cfbefc8
MohamedAmine-Hani <hani.medamine@gmail.com>
2026-03-07 22:33:06 +0000

    Challenge_Function 2

---
+++ Challenge_Function02.php
@@ -0,0 +1,13 @@
+<?php
+
+function calculateArea ($width, $height) {
+   $Area =$width*$height;
+   return($Area); 
+}
+ $totalArea = calculateArea(20, 76);
+echo "the sentence: The total area is   " .$totalArea. "   square units";
+
+
+
+
+?>
